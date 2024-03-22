f6d7cdfcd5264b2d6b1d4a030ded58ab12e3d7ce
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2024-03-22 07:54:41 -0300

    App.js update

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import TouristLocation from "./src/components/TouristLocation";
 export default function App() {
   const texts = ["Nome: Summoner's rift", "Endereço: Runeterra", "Descrição"];
   const imageUrl =
-    "https://s2-techtudo.glbimg.com/FvVp6C8s9wmFWC2YwdO096XlUXM=/0x0:2400x1708/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2018/K/y/nj1EsBR0ANgvEBN9BWvA/summoners-rift-update-map.png";
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Authentic_Viking_recreation.jpg/300px-Authentic_Viking_recreation.jpg";
 
   return (
     <View
@@ -17,9 +17,9 @@ export default function App() {
       }}
     >
       <TouristLocation imageUrl={imageUrl} />
-      <TouristLocation name="Summoner's rift" />
-      <TouristLocation address="Runeterra" />
-      <TouristLocation description="Summoner's rift" />
+      <TouristLocation name="Vinlandia" />
+      <TouristLocation address="Costa do Nordeste do Canadá" />
+      <TouristLocation description="Vinlândia foi o nome dado pelos nórdicos a uma região indeterminada da costa do Nordeste do Canadá, durante a Era Viking e a Idade Média" />
     </View>
   );
 }
